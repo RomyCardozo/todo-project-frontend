3363efe03bb55241f5cc2d3c987ebb370170390d
RomyCardozo <romycardozopy@gmail.com>
2025-11-12 20:03:05 -0300

    Primer commit del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import ListaTodoPageForm from "./pages/ListaTodoPageForm"
+import ListaTodoPage from "./pages/ListaTodoPage";
+import ListaTodoPageDetail from "./pages/ListaTodoPageDetail";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+
+  return (
+		<>
+			{/* Rutas */}
+      <nav>
+        <a href="/">Lista de Tareas</a> |{" "}
+        <a href="/create">Crear Tarea</a>
+        <a href="/edit/1"> | Editar Tarea (ID: 1)</a>
+        <a href="/1"> | Ver Detalle de Tarea (ID: 1)</a>
+      </nav>
+
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<ListaTodoPage />} />
+					<Route path="/:id" element={<ListaTodoPageDetail />} />
+					<Route path="/create" element={<ListaTodoPageForm />} />
+					<Route path="/edit/:id" element={<ListaTodoPageForm />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+  );
+}
+
+export default App
