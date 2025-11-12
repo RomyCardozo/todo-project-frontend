837af35d88b13e1bf65580092be767cb3e22556d
RomyCardozo <romycardozopy@gmail.com>
2025-11-12 20:06:26 -0300

    Agregar componentes de página: TodoForm, ListaTodoPage, ListaTodoPageDetail y ListaTodoPageForm

---
+++ src/pages/ListaTodoPageDetail.jsx
@@ -0,0 +1,11 @@
+
+const ListaTodoPageDetail = () => {
+  return (
+    <div>
+      <h1>Detalle de la Tarea</h1>
+      <p>Aquí se mostrarán los detalles de la tarea seleccionada.</p>
+    </div>
+  )
+}
+
+export default ListaTodoPageDetail
